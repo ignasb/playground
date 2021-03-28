d941d565256424479551b3f2d1b23f0b0189475c
ignasbu <home@ignasbu.lan>
2021-03-28 22:44:22 +0300

    routing to features

--- tutorials/angular/routing/routing-app/src/app/app.module.ts
+++ tutorials/angular/routing/routing-app/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { HttpClientModule } from "@angular/common/http";
-import { RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 // Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from "angular-in-memory-web-api";
-import { ProductData } from "./products/product-data";
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ProductData } from './products/product-data';
 
-import { AppComponent } from "./app.component";
-import { WelcomeComponent } from "./home/welcome.component";
-import { PageNotFoundComponent } from "./page-not-found.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 /* Feature Modules */
-import { ProductModule } from "./products/product.module";
-import { UserModule } from "./user/user.module";
-import { MessageModule } from "./messages/message.module";
+import { ProductModule } from './products/product.module';
+import { UserModule } from './user/user.module';
+import { MessageModule } from './messages/message.module';
 
 @NgModule({
   imports: [
@@ -22,19 +22,11 @@ import { MessageModule } from "./messages/message.module";
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
     ProductModule,
-    RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', component: PageNotFoundComponent },
-    ], { useHash: true }),
     UserModule,
     MessageModule,
+    AppRoutingModule,
   ],
-  declarations: [
-    AppComponent,
-    WelcomeComponent,
-    PageNotFoundComponent,
-  ],
+  declarations: [AppComponent, WelcomeComponent, PageNotFoundComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- tutorials/angular/routing/routing-app/src/app/user/user.module.ts
+++ tutorials/angular/routing/routing-app/src/app/user/user.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 import { LoginComponent } from "./login.component";
 
@@ -7,6 +8,9 @@ import { SharedModule } from "../shared/shared.module";
 @NgModule({
   imports: [
     SharedModule,
+    RouterModule.forChild([
+      { path: 'login', component: LoginComponent },
+    ]),
   ],
   declarations: [
     LoginComponent,
